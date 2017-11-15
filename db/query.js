8de2a8d25ecdbc6ccc963409d30151a6e00e8a72
leodotng <leong.jo@gmail.com>
2017-11-14 21:59:15 -0700

    completed queries

--- db/query.js
+++ db/query.js
@@ -23,14 +23,40 @@ let getBooks = () => {
 
 let addBook = (bookTitle) => {
   // add a book if the title is not in the table
+  return pg('books').select().where('title', bookTitle.title).first()
+  .then((book) => {
+    if (book === undefined) {
+      // console.log('here', bookInstance);
+      return pg('books').insert(bookTitle).returning('id')
+    } else {
+      return [book.id]
+    }
+  })
 }
 
 let addCharacter = (characterName) => {
   // add a character if the character is not in the table
+  return pg('characters').select().where('name', characterName.name).first()
+  .then((name) => {
+    if (name === undefined) {
+      return pg('characters').insert(characterName).returning('id')
+    } else {
+      return [name.id]
+    }
+  })
 }
 
 let addJoin = (book, name) => {
   // add a relationship between a character and a book if it does not exist
+  return pg('book_character').select().where('book_character.book_id', book[0]).andWhere('book_character.character_id', name[0]).first()
+  .then((join) => {
+    if (join === undefined) {
+      let add = {book_id: book[0], character_id: name[0]}
+      return pg('book_character').insert(add)
+    } else {
+      return [join.id]
+    }
+  })
 }
 
 module.exports = {
